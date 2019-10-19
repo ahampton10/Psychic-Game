b37cc18efa1ade3f2a7e3348bf4d8a69a04e7102
Alex H <hampton.alex10@gmail.com>
2019-10-19 16:11:32 -0600

    Link to js not working?

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,45 @@
-var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",]; 
 
 var wins = 0; 
 var losses = 0; 
+var numberGuesses = 9;
+var guessChoices = []; 
 
-var computerGuess = computerChoices [Math.floor(Math.random() * computerChoices.length)];
 
+document.onkeyup = function(event) {
 
+    var userGuess = event.key;
+
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)]; 
+    var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",]; 
+
+if (options.indexOf(userGuess) > -1) {
+
+    if (userGuess === computerGuess) {
+    wins++; 
+    numberGuesses = 9; 
+    GuessChoices = []; 
+}
+
+if (userGuess != computerGuess) {
+    numberGuesses --;
+    guessChoices.push(userGuess); 
+}
+
+if (numberGuesses === 0) {
+    numberGuesses = 9; 
+    losses ++; 
+    guessChoices = []; 
+}
+var html = 
+"<h1>Psychic Game </h1>" +
+"<p>Guess what letter I'm thinking of</p>" +
+"<p>Wins: " + wins + "</p>" +
+"<p>Losses: " + losses + "</p>" +
+"<p>Guesses Left: " + numberGuesses + "</p>" +
+"<p>Your guesses so far: " + guessChoices.join(", ") + "</p>";
+
+document.querySelector("#game").innerHTML = html;
+}
+}; 
